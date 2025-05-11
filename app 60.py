8b4b5b3cac1c31bbba6195699918e00000451ba4
tareqomrani <tareqomrani@outlook.com>
2025-05-10 23:02:33 -0400

    Update app 60.py

--- app 60.py
+++ app 60.py
@@ -2,7 +2,7 @@ import streamlit as st
 import time
 import os
 
-st.set_page_config(page_title="UAV Battery Efficiency Estimator", layout="centered")
+st.set_page_config(page_title="UAV Battery Efficiency Estimator", layout="centered")r
 st.title("UAV Battery Efficiency Estimator")
 
 UAV_PROFILES = {
