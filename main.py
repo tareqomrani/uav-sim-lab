a48e72879e348c955002b58e32310028c0323cf0
tareqomrani <tareqomrani@outlook.com>
2025-05-14 12:45:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,88 @@ import time
 GRAVITY = 9.81
 TIME_STEP = 10
 
-UAV_PROFILES = {
+{
+    "Generic Quad": {
+        "max_payload_g": 800,
+        "base_weight_kg": 1.2,
+        "power_system": "Battery",
+        "draw_watt": 150,
+        "battery_wh": 60,
+        "crash_risk": False
+    },
+    "DJI Phantom": {
+        "max_payload_g": 500,
+        "base_weight_kg": 1.4,
+        "power_system": "Battery",
+        "draw_watt": 120,
+        "battery_wh": 68,
+        "crash_risk": False
+    },
+    "RQ-11 Raven": {
+        "max_payload_g": 0,
+        "base_weight_kg": 1.9,
+        "power_system": "Battery",
+        "draw_watt": 90,
+        "battery_wh": 50,
+        "crash_risk": False
+    },
+    "RQ-20 Puma": {
+        "max_payload_g": 600,
+        "base_weight_kg": 6.3,
+        "power_system": "Battery",
+        "draw_watt": 180,
+        "battery_wh": 275,
+        "crash_risk": False
+    },
+    "MQ-1 Predator": {
+        "max_payload_g": 204000,
+        "base_weight_kg": 512,
+        "power_system": "Hybrid",
+        "draw_watt": 650,
+        "battery_wh": 150,
+        "crash_risk": True
+    },
+    "MQ-9 Reaper": {
+        "max_payload_g": 1700000,
+        "base_weight_kg": 2223,
+        "power_system": "Hybrid",
+        "draw_watt": 800,
+        "battery_wh": 200,
+        "crash_risk": True
+    },
+    "Skydio 2+": {
+        "max_payload_g": 150,
+        "base_weight_kg": 0.8,
+        "power_system": "Battery",
+        "draw_watt": 90,
+        "battery_wh": 45,
+        "crash_risk": False
+    },
+    "Freefly Alta 8": {
+        "max_payload_g": 9000,
+        "base_weight_kg": 6.2,
+        "power_system": "Battery",
+        "draw_watt": 400,
+        "battery_wh": 710,
+        "crash_risk": False
+    },
+    "Teal Golden Eagle": {
+        "max_payload_g": 2000,
+        "base_weight_kg": 2.2,
+        "power_system": "Hybrid",
+        "draw_watt": 220,
+        "battery_wh": 100,
+        "crash_risk": True
+    },
+    "Quantum Systems Vector": {
+        "max_payload_g": 1500,
+        "base_weight_kg": 2.3,
+        "power_system": "Battery",
+        "draw_watt": 160,
+        "battery_wh": 150,
+        "crash_risk": False
+    }
+}
     "MQ-9 Reaper": {
         "max_payload_g": 1700000,
         "base_weight_kg": 2223,
